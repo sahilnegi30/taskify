7faf02b599ff258fb9e06f0060d4de61cdc48866
sahil negi <sahilnegi658@gmail.com>
2024-09-29 22:01:25 +0530

    first commit

---
+++ backend/src/routes/tasksRoutes.js
@@ -0,0 +1,22 @@
+import express from "express";
+import {
+  createTask,
+  deleteTask,
+  getTask,
+  getTasks,
+  updateTask,
+} from "../controllers/task/taskController.js";
+import { protect } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+// router.post("/task/create", (req, res) => {
+//   res.send("task cereat");
+// });
+router.post("/task/create", protect, createTask);
+router.get("/tasks", protect, getTasks);
+router.get("/task/:id", protect, getTask);
+router.patch("/task/:id", protect, updateTask);
+router.delete("/task/:id", protect, deleteTask);
+
+export default router;
