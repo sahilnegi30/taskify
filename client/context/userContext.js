5418d0d231f1ad849562557ca36905cf8988ae8a
sahil negi <sahilnegi658@gmail.com>
2024-10-01 00:10:53 +0530

    commit

--- client/context/userContext.js
+++ client/context/userContext.js
@@ -9,7 +9,7 @@ const UserContext = React.createContext();
 axios.defaults.withCredentials = true;
 
 export const UserContextProvider = ({ children }) => {
-  const serverUrl = "https://taskfyer.onrender.com";
+  const serverUrl = "http://localhost:8000";
 
   const router = useRouter();
 
